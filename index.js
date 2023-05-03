e7ebd441dc89b017eab92d7c4424605059234607
Sam Peck <fullstacksam@protonmail.com>
2023-05-03 10:32:41 -0400

    math random is still weird

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1=31;
+let num2 =2;
+let num3 = 34;
+let num4 = 15;
 
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) +1;
+let mod = num3 % num4;
+let max = Math.max(1,2,3,20);
 // Write your code here
